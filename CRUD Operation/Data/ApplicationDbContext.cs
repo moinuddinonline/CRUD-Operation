75e394c0b709bba45328a8ac8e2034fe1a07ac93
SK MOINUDDIN <findev@vp1.in>
2024-05-03 16:59:06 +0530

    Add Some field

---
+++ CRUD Operation/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD_Operation.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Operation.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {    
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CRUD Operation/Models/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD_Operation.Models.Entities
+{
+    public class Student
+    {
+        internal Guid id;
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool Subscribed { get; set; }  
+
+    }
+}
